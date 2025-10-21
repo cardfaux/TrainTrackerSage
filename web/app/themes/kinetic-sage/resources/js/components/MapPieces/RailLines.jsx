de2b6246652f12629f8cbcfaae218e24b5a03cf4
James Hagood <cardfaux@tutanota.com>
2025-10-20 21:20:51 -0500

    I added a overlay div with a smaller z-index to be belpow the icons and tracks and stuff

--- web/app/themes/kinetic-sage/resources/js/components/MapPieces/RailLines.jsx
+++ web/app/themes/kinetic-sage/resources/js/components/MapPieces/RailLines.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Polyline } from 'react-leaflet';
 
-const RailLines = ({ linePolylines }) => (
-  <>
-    {Object.entries(linePolylines).map(([line, coords]) => (
-      <Polyline key={line} positions={coords} color="blue" weight={3} />
-    ))}
-  </>
-);
+const RailLines = ({ linePolylines }) => {
+  if (!linePolylines) return null; // guard clause
+
+  return (
+    <>
+      {Object.entries(linePolylines).map(([line, coords]) => (
+        <Polyline key={line} positions={coords} color="blue" weight={3} />
+      ))}
+    </>
+  );
+};
 
 export default RailLines;
--- web/app/themes/kinetic-sage/resources/js/components/StationsMap.jsx
+++ web/app/themes/kinetic-sage/resources/js/components/StationsMap.jsx
@@ -25,11 +25,60 @@ const StationsMap = () => {
     : [38.9, -77.03];
 
   return (
-    <MapContainer center={center} zoom={13} className="h-[500px] w-full">
-      {/* <TileLayer
-      url="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png"
-      attribution="&copy; OpenRailwayMap &copy; OpenStreetMap contributors"
-      /> */}
+    // <MapContainer center={center} zoom={13} className="h-[500px] w-full">
+    //   <TileLayer
+    //     url="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png"
+    //     attribution="&copy; OpenRailwayMap &copy; OpenStreetMap contributors"
+    //     className="night-map-tile-remove"
+    //   />
+    //   {/* <TileLayer
+    //   url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+    //   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+    //   subdomains="abcd"
+    //   /> */}
+    //   {/* <TileLayer
+    //   url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+    //   attribution="&copy; OpenStreetMap contributors &copy; CARTO"
+    //   subdomains="abcd"
+    //   /> */}
+    //   {/* <TileLayer
+    //     url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+    //     attribution="&copy; OpenRailwayMap &copy; OpenStreetMap contributors"
+    //   /> */}
+
+    //   {/* Dark overlay */}
+    //   <div
+    //     style={{
+    //       position: 'absolute',
+    //       top: 0,
+    //       left: 0,
+    //       width: '100%',
+    //       height: '100%',
+    //       pointerEvents: 'none', // so map interactions still work
+    //       background: 'rgba(0, 0, 0, 0.5)', // adjust opacity for darkness
+    //       mixBlendMode: 'multiply', // darkens tiles while keeping details
+    //       zIndex: 1000,
+    //     }}
+    //   />
+
+    //   <RailLines linePolylines={linePolylines} />
+    //   <StationMarkers stations={stations} icon={StationMarkerIcon} />
+    //   <TrainMarkers
+    //     trains={trains}
+    //     trainPositions={trainPositions}
+    //     icon={TrainMarkerIcon}
+    //   />
+    // </MapContainer>
+    <MapContainer
+      center={center}
+      zoom={13}
+      className="h-[500px] w-full relative"
+    >
+      <TileLayer
+        url="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png"
+        attribution="&copy; OpenRailwayMap &copy; OpenStreetMap contributors"
+        className="night-map-tile-remove"
+      />
       {/* <TileLayer
       url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
@@ -40,9 +89,24 @@ const StationsMap = () => {
       attribution="&copy; OpenStreetMap contributors &copy; CARTO"
       subdomains="abcd"
       /> */}
-      <TileLayer
+      {/* <TileLayer
         url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         attribution="&copy; OpenRailwayMap &copy; OpenStreetMap contributors"
+      /> */}
+
+      {/* Night-mode overlay */}
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          pointerEvents: 'none', // allows interaction with map
+          background: 'rgba(0, 0, 0, 0.5)', // adjust for darkness
+          mixBlendMode: 'multiply', // darkens tiles but keeps details
+          zIndex: 50,
+        }}
       />
 
       <RailLines linePolylines={linePolylines} />
